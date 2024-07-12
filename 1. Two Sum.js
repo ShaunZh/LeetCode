e6b70e4707c5908a473953df7b1b1c30842da2a3
ShaunZh <308826842@qq.com>
2024-07-12 09:27:41 +0800

    1. Two Sum - optimize

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -2,20 +2,16 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
- * @summary using hash table to solve it.
  */
 var twoSum = function (nums, target) {
-    // 1. use hash table store nums
-    // 2. iterate nums and use target minus the current item. 
-    // 3. check the item if exist in the map and visited.
     const map = new Map()
-    nums.forEach((it, index) => map.set(it, { index }));
     for (let i = 0, len = nums.length; i < len; i++) {
         const minusValue = target - nums[i];
         const item = map.get(minusValue);
-        if (item && item.index !== i) {
-            return [i, item.index];
+        if (item !== undefined) {
+            return [i, item];
         }
+        map.set(nums[i], i);
     }
     return [];
 };
