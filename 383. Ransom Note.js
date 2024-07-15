aadfc350a89ad2b9349be773b86998248b01918d
ShaunZh <308826842@qq.com>
2024-07-15 09:39:54 +0800

    383. Ransom Note: using array to solve it

--- 383. Ransom Note.js
+++ 383. Ransom Note.js
@@ -1,3 +1,28 @@
+
+
+/**
+ * @description: Recommend this solution, becase array is more efficient than object.
+ * @param {*} ransomNote
+ * @param {*} magazine
+ * @return {*}
+ */
+var canConstruct_using_array = function (ransomNote, magazine) {
+    const arr = new Array(26).fill(0);
+
+    for (let i = 0, len = magazine.length; i < len; i++) {
+        const utf16code = magazine[i].charCodeAt() - 97;
+        arr[utf16code] += 1;
+    }
+    for (let i = 0, len = ransomNote.length; i < len; i++) {
+        const utf16code = ransomNote[i].charCodeAt() - 97;
+        if (!arr[utf16code]) {
+            return false;
+        }
+        arr[utf16code] -= 1;
+    }
+    return true;
+};
+
 /**
  * @param {string} ransomNote
  * @param {string} magazine
@@ -8,7 +33,7 @@
  * 3. if not, return false
  * 4. if the letter is in the object, minus one;
  */
-var canConstruct = function (ransomNote, magazine) {
+var canConstruct_using_object = function (ransomNote, magazine) {
     const objMagazine = {};
 
     for (let i = 0, len = magazine.length; i < len; i++) {
--- algorithms/hashTable.md
+++ algorithms/hashTable.md
@@ -5,8 +5,14 @@ In JavaScript, there is three data type we can select:
 * map
 * object
 
-if the size of data is less than 1000, we should use `array`;
+When we use `Array`?
+1. if the size of data is less than 1000, we should use `array`;
+2. if we need to calculate the sum of each single letter, we should use array to store the sum. Using the UTF-16 code of letter as the key of array, the sum of letter as the value of array. 
+(Note: we can use `string.chartCodeAt()` to get the UTF-16 code, if the letters are all lowercase lettters,we can get the key by using `const key = string.charCodeAt() - a.charCodeAt()`, the letter `a` is the first letter, so we have to minus its UTF-16 code.). In order to understand the key point, you can check the problem [383. Ransom Note](https://leetcode.com/problems/ransom-note/description/) of LeetCode.
+
 
 
 什么时候使用哈希法? 
 当我们需要查询一个元素是否出现过，或者一个元素是否在集合里的时候，就要第一时间想到哈希法。
+
+
